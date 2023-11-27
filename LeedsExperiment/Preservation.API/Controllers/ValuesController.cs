d75d80f7be149f122783b5683fa5d8c472284ae6
tomcrane <tom@nanoplanet.com>
2023-11-27 09:55:51 +0000

    make a / route for API

---
+++ LeedsExperiment/Preservation.API/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Preservation.API.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "I'm alive";
+        }
+    }
+}
