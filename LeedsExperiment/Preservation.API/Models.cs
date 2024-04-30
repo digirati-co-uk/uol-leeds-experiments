80aafc32a68ba38619e8d771f7e0f529387fa50a
Donald Gray <donald.gray@digirati.com>
2024-04-30 14:27:30 +0100

    [JsonPropertyOrder] for preservation api

--- LeedsExperiment/Preservation.API/Models.cs
+++ LeedsExperiment/Preservation.API/Models.cs
@@ -14,14 +14,19 @@ public abstract class PreservationResource
     [JsonPropertyOrder(1)]
     public Uri? Id { get; set; }
     
+    [JsonPropertyOrder(2)]
     public abstract string Type { get; set; }
     
+    [JsonPropertyOrder(97)]
     public DateTime Created { get; set; }
     
+    [JsonPropertyOrder(98)]
     public Uri CreatedBy { get; set; }
     
+    [JsonPropertyOrder(99)]
     public DateTime? LastModified { get; set; }
     
+    [JsonPropertyOrder(100)]
     public Uri? LastModifiedBy { get; set; }
 }
 
@@ -41,27 +46,32 @@ public class DigitalObject : PreservationResource
     /// <summary>
     /// Name of this <see cref="DigitalObject"/>
     /// </summary>
-    public string Name { get; set; }
+    [JsonPropertyOrder(3)]
+    public string? Name { get; set; }
     
     /// <summary>
     /// Currently rendered version of object 
     /// </summary>
-    public DigitalObjectVersion Version { get; set; }
+    [JsonPropertyOrder(10)]
+    public DigitalObjectVersion? Version { get; set; }
     
     /// <summary>
     /// Previous versions of object 
     /// </summary>
-    public DigitalObjectVersion[] Versions { get; set; }
+    [JsonPropertyOrder(11)]
+    public DigitalObjectVersion[]? Versions { get; set; }
     
     /// <summary>
     /// A list of the immediate child containers, if any.
     /// </summary>
-    public Container[] Containers { get; set; }
+    [JsonPropertyOrder(12)]
+    public Container[]? Containers { get; set; }
     
     /// <summary>
     /// A list of the immediate contained binaries, if any.
     /// </summary>
-    public Binary[] Binaries { get; set; }
+    [JsonPropertyOrder(12)]
+    public Binary[]? Binaries { get; set; }
 }
 
 public class DigitalObjectVersion
@@ -76,11 +86,13 @@ public class DigitalObjectVersion
     /// <summary>
     /// Name of this version (e.g. v1, v2 etc)
     /// </summary>
-    public string Name { get; set; }
+    [JsonPropertyOrder(2)]
+    public string? Name { get; set; }
     
     /// <summary>
     /// Date of this version
     /// </summary>
+    [JsonPropertyOrder(3)]
     public DateTime Date { get; set; }
 }
 
@@ -98,21 +110,25 @@ public class Container : PreservationResource
     /// The original name, which may contain any UTF-8 character. Often this will be the same as the last path element
     /// of the @id, but it does not have to be.
     /// </summary>
-    public string Name { get; set; }
+    [JsonPropertyOrder(2)]
+    public string? Name { get; set; }
     
     /// <summary>
     /// A list of the immediate child containers, if any.
     /// </summary>
+    [JsonPropertyOrder(10)]
     public Container[] Containers { get; set; }
     
     /// <summary>
     /// A list of the immediate contained binaries, if any.
     /// </summary>
+    [JsonPropertyOrder(11)]
     public Binary[] Binaries { get; set; }
     
     /// <summary>
     /// The @id of the DigitalObject the Container is in. Not present if the Container is outside a DigitalObject.
     /// </summary>
+    [JsonPropertyOrder(12)]
     public Uri? PartOf { get; set; }
 }
 
@@ -129,32 +145,37 @@ public class Binary : PreservationResource
     /// <summary>
     /// The original name, which may contain any UTF-8 character.
     /// </summary>
-    public string Name { get; set; }
+    [JsonPropertyOrder(3)]
+    public string? Name { get; set; }
     
     /// <summary>
     /// The SHA-256 checksum for this file. This will always be returned by the API, but is only required when sending
     /// to the API if the checksum is not provided some other way
     /// </summary>
-    public string Digest { get; set; }
+    [JsonPropertyOrder(10)]
+    public string? Digest { get; set; }
     
     /// <summary>
     /// The S3 URI within a Deposit where this file may be accessed. If just browsing, this will be empty. If importing
     /// and sending this data to the API as part of an ImportJob, this is the S3 location the API should read the file
     /// from.
     /// </summary>
-    public Uri Location { get; set; }
+    [JsonPropertyOrder(11)]
+    public Uri? Location { get; set; }
     
     /// <summary>
     /// An endpoint from which the binary content of the file may be retrieved (subject to authorisation). This is
     /// always provided by the API for API users to read a single file (it's not a location for the API to fetch from)
     /// </summary>
-    public Uri Content { get; set; }
+    [JsonPropertyOrder(12)]
+    public Uri? Content { get; set; }
     
     /// <summary>
     /// The @id of the DigitalObject the Binary is in. Never null when returned by the API. Not required when sending as
     /// part of an ImportJob.
     /// </summary>
-    public Uri PartOf { get; set; }
+    [JsonPropertyOrder(13)]
+    public Uri? PartOf { get; set; }
 }
 
 /// <summary>
@@ -175,48 +196,57 @@ public class Deposit : PreservationResource
     /// users, it will be assigned automatically. It may suit you to set this shortly before sending the deposit for
     /// preservation.
     /// </summary>
+    [JsonPropertyOrder(10)]
     public Uri DigitalObject { get; set; }
     
     /// <summary>
     /// An S3 key that represents a parent location. Use the "space" under this key to assemble files for an ImportJob.
     /// </summary>
+    [JsonPropertyOrder(11)]
     public string Files { get; set; } 
     
     /// <summary>
     /// TBC - a step in a workflow.
     /// </summary>
+    [JsonPropertyOrder(12)]
     public string Status { get; set; }
     
     /// <summary>
     /// A space to leave notes for colleagues or your future self
     /// </summary>
+    [JsonPropertyOrder(13)]
     public string SubmissionText { get; set; }
     
     /// <summary>
     /// Timestamp indicating when this deposit was last used to create an ImportJob for the Repository
     /// </summary>
+    [JsonPropertyOrder(14)]
     public DateTime DatePreserved { get; set; }
     
     /// <summary>
     /// If this deposit was created as a result of asking the API to export a DigitalObject, the date that happened.
     /// </summary>
+    [JsonPropertyOrder(15)]
     public DateTime? DateExported { get; set; }
     
     /// <summary>
     /// If this deposit was created as a result of asking the API to export a DigitalObject, the version of the Digital
     /// object that was exported then.
     /// </summary>
+    [JsonPropertyOrder(16)]
     public DigitalObjectVersion? VersionExported { get; set; }
     
     /// <summary>
     /// If an Import Job is created from the files in this deposit and then sent to Preservation, the version that was
     /// created.
     /// </summary>
+    [JsonPropertyOrder(17)]
     public DigitalObjectVersion? VersionSaved { get; set; }
     
     /// <summary>
     /// A list of jobs that have run on this deposit (TBC)
     /// </summary>
+    [JsonPropertyOrder(18)]
     public string[] PipelineJobs { get; set; }
 }
 
@@ -227,6 +257,7 @@ public class ImportJob : PreservationResource
     /// <summary>
     /// The Deposit that was used to generate this job, and to which it will be sent if executed.
     /// </summary>
+    [JsonPropertyOrder(10)]
     public Uri Deposit { get; set; }
     
     /// <summary>
@@ -234,12 +265,14 @@ public class ImportJob : PreservationResource
     /// this job might be creating it. The value must match the digitalObject of the deposit, so it's technically
     /// redundant, but must be included so that the intent is explicit and self-contained.
     /// </summary>
+    [JsonPropertyOrder(11)]
     public Uri DigitalObject { get; set; }
     
     /// <summary>
     /// Always provided when you ask the API to generate an ImportJob as a diff and the DigitalObject already exists.
     /// May be null for a new object
     /// </summary>
+    [JsonPropertyOrder(12)]
     public DigitalObjectVersion? SourceVersion { get; set; }
     
     /// <summary>
@@ -248,6 +281,7 @@ public class ImportJob : PreservationResource
     /// permitted set. The name property of the container may be any UTF-8 characters, and can be used to preserve an
     /// original directory name.
     /// </summary>
+    [JsonPropertyOrder(13)]
     public Container[] ContainersToAdd { get; set; }
     
     /// <summary>
@@ -259,17 +293,20 @@ public class ImportJob : PreservationResource
     /// include this field. Note that in the second example above, the URI last path element, the name property, and the
     /// S3 location last path element are all different - this is permitted, although perhaps unusual.
     /// </summary>
+    [JsonPropertyOrder(14)]
     public Binary[] BinariesToAdd { get; set; }
     
     /// <summary>
     /// A list of containers to remove. @id is the only required property. The Containers must either be already empty,
     /// or only contain Binaries mentioned in the binariesToDelete property of the same ImportJob.
     /// </summary>
+    [JsonPropertyOrder(15)]
     public Container[] ContainersToDelete { get; set; }
     
     /// <summary>
     /// A list of binaries to remove. @id is the only required property.
     /// </summary>
+    [JsonPropertyOrder(16)]
     public Binary[] BinariesToDelete { get; set; }
     
     /// <summary>
@@ -279,6 +316,7 @@ public class ImportJob : PreservationResource
     /// supplied name. The location must be an S3 key within the Deposit. The digest is only required if the SHA256
     /// cannot be obtained by the API from METS file information or from S3 metadata.
     /// </summary>
+    [JsonPropertyOrder(17)]
     public Binary[] BinariesToPatch { get; set; }
 }
 
@@ -291,72 +329,86 @@ public class ImportJobResult : PreservationResource
     /// minted by the API when you actually submit an ImportJob, because: 1) not all Import Jobs are actually executed;
     /// 2) It may have been the special .../diff ImportJob; 3) It may have been an external identifier you provided.
     /// </summary>
+    [JsonPropertyOrder(10)]
     public Uri ImportJob { get; set; }
     
     /// <summary>
     /// The @id property of the original submitted job
     /// </summary>
+    [JsonPropertyOrder(11)]
     public Uri OriginalImportJobId { get; set; }
     
     /// <summary>
     /// Explicitly included for convenience; the deposit the job was started from.
     /// </summary>
+    [JsonPropertyOrder(12)]
     public Uri Deposit { get; set; }
     
     /// <summary>
     /// Also included for convenience, the repository object the changes specified in the job are being applied to
     /// </summary>
+    [JsonPropertyOrder(13)]
     public Uri DigitalObject { get; set; }
 
     /// <summary>
     /// One of "waiting", "running", "completed", "completedWithErrors"
     /// </summary>
     /// <remarks>Should this be an enum?</remarks>
+    [JsonPropertyOrder(14)]
     public string Status { get; set; }
     
     /// <summary>
     /// Timestamp indicating when the API started processing the job. Will be null/missing until then.
     /// </summary>
+    [JsonPropertyOrder(15)]
     public DateTime? DateBegun { get; set; }
     
     /// <summary>
     /// Timestamp indicating when the API finished processing the job. Will be null/missing until then.
     /// </summary>
+    [JsonPropertyOrder(16)]
     public DateTime? DateFinished { get; set; }
     
     /// <summary>
     /// The version of the DigitalObject this job caused to be produced. Not known until the job has finished processing
     /// </summary>
+    [JsonPropertyOrder(17)]
     public DigitalObjectVersion? NewVersion { get; set; }
     
     /// <summary>
     /// A list of errors encountered. These are error objects, not strings. 
     /// </summary>
+    [JsonPropertyOrder(18)]
     public Error[]? Errors { get; set; }
     
     /// <summary>
     /// Populated once the job has finished successfully.
     /// </summary>
+    [JsonPropertyOrder(19)]
     public Container[]? ContainersAdded { get; set; }
     
     /// <summary>
     /// Populated once the job has finished successfully.
     /// </summary>
+    [JsonPropertyOrder(20)]
     public Binary[]? BinariesAdded { get; set; }
     
     /// <summary>
     /// Populated once the job has finished successfully.
     /// </summary>
+    [JsonPropertyOrder(21)]
     public Container[]? ContainersDeleted { get; set; }
     
     /// <summary>
     /// Populated once the job has finished successfully.
     /// </summary>
+    [JsonPropertyOrder(22)]
     public Binary[]? BinariesDeleted { get; set; }
     
     /// <summary>
     /// Populated once the job has finished successfully.
     /// </summary>
+    [JsonPropertyOrder(23)]
     public Binary[]? BinariesPatched { get; set; }
 }
 
