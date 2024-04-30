c9647f348687e0692f1cad72f44ea44b76de7a43
Donald Gray <donald.gray@digirati.com>
2024-04-30 14:53:24 +0100

    Add swagger to preservation api

--- LeedsExperiment/Preservation.API/Program.cs
+++ LeedsExperiment/Preservation.API/Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 using Preservation;
 using PreservationApiClient;
 
@@ -21,7 +23,20 @@ builder.Services.AddHttpClient<IPreservation, StorageService>(client =>
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 
-// TODO - swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UoL Demo Preservation API",
+        Version = "v0.1",
+        Description =
+            "Preservation API is consumed by the applications we are going to build. For demo/test purposes only, focussed on 'happy path' (minimal error handling etc)."
+    });
+    
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
@@ -40,6 +55,8 @@ app.MapGet("/deposits/{id}", (string id) => $"Get details of Deposit {id}");
 app.MapGet("/deposits/{id}/importJobs/diff", (string id) => $"Get importJob JSON for files in Deposit {id}");
 app.MapPost("/deposits/{id}/importJobs", (string id) => "Import data into Fedora");
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.UseHttpLogging();
